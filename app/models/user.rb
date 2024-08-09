6cf5c527ce358c687edbbc68806421bf4b8f58e7
MarkusMachel <machelmarkus@gmail.com>
2024-08-08 21:39:01 -0300

    Make User model (including secure passwords)

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  before_save { email.downcase! }
+
+  validates :name, presence: true, 
+    length: { maximum: 50 }
+
+  validates :email, presence: true, 
+    length: { maximum: 255 }, 
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { uniqueness: true }
+
+  validates :password, length: { minimum: 6 },
+    presence: true
+    
+  has_secure_password
+end
