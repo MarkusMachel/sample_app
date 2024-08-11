1221a309882850e542cdb2fe455aed1150187340
MarkusMachel <machelmarkus@gmail.com>
2024-08-11 19:16:24 -0300

    Finish user sign u[

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
-      #handle successfull save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
-      render 'new', status: unprocessable_entity
+      render 'new', status: :unprocessable_entity
     end
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
